17b33e750141b36f18a079f5c1aeb4e54f656688
xuchenhao001 <xu.chenhao@hotmail.com>
2024-11-16 17:12:25 +1100

    fix bug in autoscript

--- auto_scripts/default.sh
+++ auto_scripts/default.sh
@@ -4,8 +4,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DRAG_DIR=$(dirname $SCRIPT_DIR)
 
 cd $DRAG_DIR
-nohup python drag.py --log_level=INFO \
+nohup python drag.py \
     --config="./config/drag.yaml" \
     --config="./config/model/llama32_3b.yaml" \
     --config="./config/dataset/trivia_qa.yaml" \
-    > $DRAG_DIR/script.log 2>&1 &
+    --log_level=INFO > $DRAG_DIR/script.log 2>&1 &
+
